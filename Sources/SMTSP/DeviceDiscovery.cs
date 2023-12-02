cd241a0429ba1064b081c6b4d114a8b65803b83d
Julian Baumann <git@julba.de>
2023-12-02 16:05:10 +0100

    Test CI

--- Sources/SMTSP/DeviceDiscovery.cs
+++ Sources/SMTSP/DeviceDiscovery.cs
@@ -5,21 +5,22 @@ namespace SMTSP;
 
 public class DeviceDiscovery
 {
-    private readonly Device _myDevice;
-    private readonly BonjourDiscovery _innerDiscoveryService;
+    // private readonly Device _myDevice;
+    // private readonly BonjourDiscovery _innerDiscoveryService;
     private readonly BleDiscovery _bleDiscovery = new();
 
     public ObservableCollection<Device> DiscoveredDevices { get; }
 
     public DeviceDiscovery(Device myDevice)
     {
-        _myDevice = myDevice;
-        _innerDiscoveryService = new BonjourDiscovery(myDevice);
-        DiscoveredDevices = _innerDiscoveryService.DiscoveredDevices;
+        // _myDevice = myDevice;
+        // _innerDiscoveryService = new BonjourDiscovery(myDevice);
+        // DiscoveredDevices = _innerDiscoveryService.DiscoveredDevices;
+        DiscoveredDevices = _bleDiscovery.DiscoveredDevices;
     }
 
     public void Browse()
     {
-        _innerDiscoveryService.Browse();
+        _bleDiscovery.Browse();
     }
 }
